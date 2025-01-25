c6ca918c958858192ad4e085266fe52dac20522c
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-01-25 17:47:29 -0300

    feat: enhance Tailwind CSS configuration with additional spacing and font size options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      spacing: {
+        4.5: '1.125rem',
+      },
+      maxWidth: {
+        'screen-lg': '1112px',
+      },
+      fontSize: {
+        '5xl': '3.25rem',
+        '5.5xl': '3.5rem',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
