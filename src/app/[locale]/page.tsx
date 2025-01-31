bc0d21cc7f878e26305d45065d8099dd132ff20c
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-01-31 17:50:28 -0300

    feat: add header

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
-import { getTranslations, setRequestLocale } from 'next-intl/server'
-import Image from 'next/image'
+import { setRequestLocale } from 'next-intl/server'
+import Link from 'next/link'
 
 type HomeProps = {
   params: Promise<{ locale: string }>
@@ -9,106 +9,36 @@ export default async function Home({ params }: HomeProps) {
   const { locale } = await params
   setRequestLocale(locale)
 
-  const t = await getTranslations('home')
-
   return (
-    <div className='min-h-dvh flex flex-col relative overflow-hidden max-h-dvh'>
-      <header className='bg-[#161C51] p-6 sm:p-4.5 flex justify-center items-center'>
-        <p className='text-sm text-[#F1F2FF] font-normal uppercase max-sm:max-w-56'>
-          {t.rich('header', {
-            book: (chunks) => (
-              <a
-                href='/doc/book.pdf'
-                className='inline-flex items-center gap-0.5'
-              >
-                <span className='underline decoration-wavy underline-offset-4 decoration-from-font text-white'>
-                  {chunks}
-                </span>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='14'
-                  height='14'
-                  fill='none'
-                  viewBox='0 0 14 14'
-                >
-                  <path
-                    fill='#fff'
-                    d='m3.733 10.5-.816-.817 5.6-5.6H3.5V2.917h7v7H9.333V4.9l-5.6 5.6Z'
-                  />
-                </svg>
-              </a>
-            ),
-          })}
-        </p>
-      </header>
+    <div className='min-h-dvh flex flex-col relative overflow-hidden'>
+      <header className='flex justify-between items-center px-20 py-5 font-normal text-[#282828] text-base'>
+        <nav className='space-x-4'>
+          <Link href='/' className='font-medium'>
+            Projets
+          </Link>
+          <Link href='/doc/book.pdf'>CV</Link>
+        </nav>
 
-      <main className='max-w-screen-lg px-11 py-14 sm:py-20 flex-1 flex flex-col justify-end xl:ml-[12.5rem]'>
-        <Image
-          role='presentation'
-          src='/blur-bg.webp'
-          alt=''
-          width={1512}
-          height={982}
-          priority
-          className='pointer-events-none absolute right-0 top-0 w-full h-screen object-cover select-none -z-10'
-        />
-        <Image
-          role='presentation'
-          src='/maelle-bg.webp'
-          alt=''
-          width={2578}
-          height={659}
-          unoptimized
-          priority
-          className='pointer-events-none absolute -left-24 sm:-left-12 bottom-16 object-cover object-left min-w-max w-full 2xl:min-w-[171vw] select-none -z-10 mix-blend-multiply supports-[mix-blend-mode:plus-darker]:mix-blend-plus-darker'
-        />
-        <div className='mb-6 flex gap-4'>
-          <a
-            href='https://www.linkedin.com/in/maellems/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-sm text-[#11187F] p-3 bg-white bg-opacity-40 rounded-lg leading-none border border-white gap-2 inline-flex items-center backdrop-blur-sm'
-            aria-label='Visit LinkedIn profile'
-          >
+        <h1>Maëlle - Digital Product Designer</h1>
+
+        <div className='flex gap-5 items-center'>
+          <button>EN</button>
+          <button>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
+              width='20'
+              height='20'
               fill='none'
-              viewBox='0 0 16 16'
+              viewBox='0 0 20 20'
             >
               <path
-                fill='#0C1354'
-                d='M3.322 4.798a1.459 1.459 0 1 0 0-2.917 1.459 1.459 0 0 0 0 2.917ZM6.158 5.903v8.093h2.513V9.994c0-1.056.198-2.079 1.508-2.079 1.291 0 1.307 1.208 1.307 2.146v3.936H14V9.559c0-2.18-.47-3.856-3.017-3.856-1.224 0-2.044.672-2.379 1.307H8.57V5.903H6.158Zm-4.095 0H4.58v8.093H2.063V5.903Z'
+                fill='#000'
+                d='M10 17.5c-2.083 0-3.854-.73-5.313-2.188C3.23 13.854 2.5 12.084 2.5 10s.73-3.854 2.188-5.313C6.146 3.23 7.917 2.5 10 2.5a7.77 7.77 0 0 1 1.125.083A4.414 4.414 0 0 0 9.76 4.157a4.415 4.415 0 0 0-.51 2.093c0 1.25.438 2.313 1.313 3.188.874.874 1.937 1.312 3.187 1.312.764 0 1.465-.17 2.104-.51a4.443 4.443 0 0 0 1.563-1.365A7.106 7.106 0 0 1 17.5 10c0 2.083-.73 3.854-2.188 5.313C13.854 16.77 12.084 17.5 10 17.5Zm0-1.667c1.222 0 2.32-.337 3.292-1.01a5.848 5.848 0 0 0 2.125-2.636 8.87 8.87 0 0 1-.834.167 5.618 5.618 0 0 1-.833.063c-1.708 0-3.163-.601-4.365-1.803C8.183 9.412 7.583 7.958 7.583 6.25c0-.278.021-.556.063-.833.042-.278.097-.556.167-.834a5.847 5.847 0 0 0-2.636 2.125c-.674.973-1.01 2.07-1.01 3.292 0 1.611.57 2.986 1.708 4.125 1.139 1.139 2.514 1.708 4.125 1.708Z'
               />
             </svg>
-          </a>
-          <a
-            href='mailto:moisismaelle@gmail.com'
-            className='text-sm text-[#11187F] p-3 bg-white bg-opacity-40 rounded-lg leading-none border border-white gap-2 inline-flex items-center backdrop-blur-sm'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='none'
-              viewBox='0 0 16 16'
-            >
-              <path
-                fill='#0C1354'
-                d='M13.333 2.667H2.667c-.736 0-1.334.598-1.334 1.333v8c0 .735.598 1.333 1.334 1.333h10.666c.736 0 1.334-.598 1.334-1.333V4c0-.735-.598-1.333-1.334-1.333Zm0 1.333v.34L8 8.49 2.667 4.34V4h10.666ZM2.667 12V6.03L7.59 9.86a.663.663 0 0 0 .818 0l4.924-3.83.002 5.97H2.667Z'
-              />
-            </svg>
-
-            <span>moisismaelle@gmail.com</span>
-          </a>
+          </button>
         </div>
-
-        <h1 className='inline-block font-normal text-5xl sm:text-5.5xl italic text-[#0C1354] max-w-5xl leading-[56px] sm:leading-[65px] text-balance'>
-          Digital product designer based in Paris focused on clean and
-          user-centered solutions
-        </h1>
-      </main>
+      </header>
     </div>
   )
 }
