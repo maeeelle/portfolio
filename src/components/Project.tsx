16d267c3dfccbbe8ff20bc78f507c16a5b9cc64c
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-01 11:26:02 -0300

    feat(Project): add ArrowIcon component and enhance project title with hover effects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -14,11 +14,29 @@ function ProjectTag({ name }: ProjectTagProps) {
   )
 }
 
+function ArrowIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='32'
+      height='32'
+      fill='none'
+      viewBox='0 0 32 32'
+      className='transition-all duration-300 opacity-0 -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0'
+    >
+      <path
+        fill='#0400FF'
+        d='M8 8v2.667h11.453L6.667 23.453l1.88 1.88 12.786-12.786V24H24V8H8Z'
+      />
+    </svg>
+  )
+}
+
 export function Project({ organization, project, tags, id }: Project) {
   const titleId = `project-${id}-title`
 
   return (
-    <Link href={`/${id}`} aria-labelledby={titleId} className='block'>
+    <Link href={`/${id}`} aria-labelledby={titleId} className='block group'>
       <article aria-labelledby={titleId} className='font-[350] pr-12'>
         <header>
           <div className='text-[#161616] text-base mb-4 inline-flex items-center gap-2'>
@@ -36,8 +54,12 @@ export function Project({ organization, project, tags, id }: Project) {
             </div>
           </div>
           <div>
-            <h2 id={titleId} className='text-4.5xl/none text-black mb-2'>
+            <h2
+              id={titleId}
+              className='text-4.5xl/none text-black mb-2 inline-flex items-center gap-2 group-hover:text-[#0400FF] transition-colors duration-150'
+            >
               {project.title}
+              <ArrowIcon />
             </h2>
             <p className='text-[#8E8E8E] text-2xl/none'>{project.subtitle}</p>
           </div>
