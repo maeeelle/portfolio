1a592f5d9cce73f4b2c0fd8939fb8c75efda9608
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-01 14:15:30 -0300

    fix: show light mode first

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -49,7 +49,7 @@ export default async function Home({ params }: HomeProps) {
               height='20'
               fill='none'
               viewBox='0 0 20 20'
-              className='absolute inset-0 transition-all duration-300 group-hover:translate-y-5 opacity-100 group-hover:opacity-0'
+              className='absolute inset-0 transition-all duration-300 -translate-y-5 opacity-0 group-hover:translate-y-0 group-hover:opacity-100'
               aria-hidden='true'
             >
               <path
@@ -63,7 +63,7 @@ export default async function Home({ params }: HomeProps) {
               width='20'
               height='20'
               viewBox='0 0 24 24'
-              className='absolute inset-0 transition-all duration-300 -translate-y-5 opacity-0 group-hover:translate-y-0 group-hover:opacity-100'
+              className='absolute inset-0 transition-all duration-300 group-hover:translate-y-5 opacity-100 group-hover:opacity-0'
               aria-hidden='true'
             >
               <path d='M12 15q1.25 0 2.125-.875T15 12t-.875-2.125T12 9t-2.125.875T9 12t.875 2.125T12 15m0 2q-2.075 0-3.537-1.463T7 12t1.463-3.537T12 7t3.538 1.463T17 12t-1.463 3.538T12 17m-7-4H1v-2h4zm18 0h-4v-2h4zM11 5V1h2v4zm0 18v-4h2v4zM6.4 7.75 3.875 5.325 5.3 3.85l2.4 2.5zm12.3 12.4-2.425-2.525L17.6 16.25l2.525 2.425zM16.25 6.4l2.425-2.525L20.15 5.3l-2.5 2.4zM3.85 18.7l2.525-2.425L7.75 17.6l-2.425 2.525zM12 12' />
