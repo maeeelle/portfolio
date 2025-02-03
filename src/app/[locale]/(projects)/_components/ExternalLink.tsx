00c3c28d8c6600658cde6b75eff4d4aa6ca3c4f9
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-03 02:31:36 -0300

    fix: adjust padding for ExternalLink component for better spacing

--- src/app/[locale]/(projects)/_components/ExternalLink.tsx
+++ src/app/[locale]/(projects)/_components/ExternalLink.tsx
@@ -9,7 +9,7 @@ export function ExternalLink({ href, children }: ExternalLinkProps) {
   return (
     <Link
       href={href}
-      className='p-4 mt-10 rounded inline-block leading-none font-[350] -tracking-[.02em] text-[#F9F9F9] bg-[#161616] dark:bg-[#F9F9F9] dark:text-[#161616]'
+      className='px-4 py-3.5 mt-10 rounded inline-block leading-none font-[350] -tracking-[.02em] text-[#F9F9F9] bg-[#161616] dark:bg-[#F9F9F9] dark:text-[#161616]'
       target='_blank'
       rel='noopener noreferrer'
     >
