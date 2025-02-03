cf386bb5ff68334b5504a62899e1ac28f1cc8cd7
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-03 02:02:52 -0300

    feat: add Totoro project page with detailed project information and images

---
+++ src/app/[locale]/(projects)/totoro/page.tsx
@@ -0,0 +1,154 @@
+import Image, { ImageProps } from 'next/image'
+import { ProjectTag, ProjectTagList } from '@/components/Project'
+import { setRequestLocale } from 'next-intl/server'
+import { Heading } from '../_components/Heading'
+import { Paragraph } from '../_components/Paragraph'
+import { BackButton } from '../_components/BackButton'
+import { ProjectHeader } from '../_components/ProjectHeader'
+import { Container } from '../_components/Container'
+import { Main } from '../_components/Main'
+import { ProjectFooter } from '../_components/Footer'
+import { Aside } from '../_components/Aside'
+import { ProjectArticle } from '../_components/ProjectArticle'
+import { ExternalLink } from '../_components/ExternalLink'
+
+type PageProps = {
+  params: Promise<{
+    locale: string
+  }>
+}
+
+function ProjectImage(props: ImageProps) {
+  return (
+    // eslint-disable-next-line jsx-a11y/alt-text
+    <Image
+      {...props}
+      className='rounded-lg w-full select-none border border-black/10 dark:border-white/10'
+    />
+  )
+}
+
+export default async function ProjectPage({ params }: PageProps) {
+  const { locale } = await params
+  setRequestLocale(locale)
+
+  return (
+    <Container>
+      <Main>
+        <BackButton />
+
+        <ProjectArticle>
+          <div>
+            <ProjectHeader
+              logoSrc='/images/projects/hetic.png'
+              logoAlt='HETIC logo'
+              company='HETIC'
+              title='Totoro'
+              subtitle='Recréer du lien à travers l’entraide entre voisins'
+            />
+
+            <div className='mt-8 space-y-10 xl:space-y-8'>
+              <section>
+                <Heading>Contexte</Heading>
+                <Paragraph>
+                  Totoro est une application visant à favoriser davantage les
+                  échanges de proximité et à redynamiser la vie en communauté.
+                </Paragraph>
+                <Paragraph>
+                  En guise de récompense suite à la réalisation d’une mission
+                  locale, l’utilisateur gagne des tokens qu’il peut ensuite
+                  échanger contre des bons de réductions à utiliser chez les
+                  commerçants proches de chez lui.
+                </Paragraph>
+              </section>
+              <section>
+                <Heading>Contribution</Heading>
+                <Paragraph>
+                  Lors de ce projet, j’ai eu l’occasion de réaliser l’identité
+                  visuelle. J’ai également co-construit, en binôme avec le
+                  second designer, le parcours utilisateur ainsi qu’un design
+                  kit et l’ensemble des écrans de l’application.
+                </Paragraph>
+              </section>
+              <section>
+                <Heading>Equipe</Heading>
+
+                <ul className='text-base text-pretty leading-normal xl:leading-[25px] font-[350] text-[#282828] dark:text-[#D3D3D3]'>
+                  <li>
+                    <a className='text-[#4F32FF] dark:text-[#B2FF5D]'>
+                      Jarce Boukoro
+                    </a>{' '}
+                    - Développeur Fullstack
+                  </li>
+                  <li>
+                    <a className='text-[#4F32FF] dark:text-[#B2FF5D]'>
+                      Maëlle Moisis
+                    </a>{' '}
+                    - UX/UI Designer
+                  </li>
+
+                  <li>
+                    <a className='text-[#4F32FF] dark:text-[#B2FF5D]'>
+                      Maxence Wolff
+                    </a>{' '}
+                    - UI Designer + Développeur Front-End
+                  </li>
+                  <li>
+                    <a className='text-[#4F32FF] dark:text-[#B2FF5D]'>
+                      Téo Lugat
+                    </a>{' '}
+                    - Développeur Fullstack
+                  </li>
+                </ul>
+              </section>
+            </div>
+
+            <ExternalLink href=''>Accéder au Figma</ExternalLink>
+          </div>
+
+          <ProjectFooter year='2022'>
+            <ProjectTagList>
+              <ProjectTag name='UX' />
+              <ProjectTag name='UI' />
+              <ProjectTag name='Application mobile' />
+              <ProjectTag name='Branding' />
+            </ProjectTagList>
+          </ProjectFooter>
+        </ProjectArticle>
+      </Main>
+
+      <Aside>
+        <ProjectImage
+          width={3840}
+          height={2160}
+          src='/images/projects/totoro/cover.webp'
+          alt=''
+        />
+        <ProjectImage
+          width={3240}
+          height={1317}
+          src='/images/projects/totoro/styleguide.webp'
+          alt=''
+        />
+        <ProjectImage
+          width={2466}
+          height={1624}
+          src='/images/projects/totoro/pages-1.webp'
+          alt=''
+        />
+        <ProjectImage
+          width={2388}
+          height={1624}
+          src='/images/projects/totoro/pages-2.webp'
+          alt=''
+        />
+        <ProjectImage
+          width={2410}
+          height={1624}
+          src='/images/projects/totoro/pages-3.webp'
+          alt=''
+        />
+      </Aside>
+    </Container>
+  )
+}
