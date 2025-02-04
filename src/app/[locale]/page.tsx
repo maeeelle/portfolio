bbde516760fadd76deabbc87368cb57d46f0a945
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-04 11:13:07 -0300

    feat:fix h1 wrong margin and enhance CV link styling

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -17,13 +17,11 @@ export default async function Home({ params }: HomeProps) {
     <div className='min-h-dvh flex flex-col relative overflow-hidden'>
       <header className='max-w-screen-2xl mx-auto w-full flex justify-between items-center p-8 sm:px-20 font-normal text-[#282828] dark:text-[#D3D3D3] text-sm'>
         <div className='flex gap-2 items-center'>
-          <h1 className='max-sm:-order-1 w-full sm:w-auto mb-1.5 sm:mb-0'>
-            MAËLLE.WORKS
-          </h1>
+          <h1>MAËLLE.WORKS</h1>
           <span>•</span>
           <Link
             href='/doc/cv_maelle.pdf'
-            className='text-[#4F32FF] dark:text-[#B2FF5D]'
+            className='no-underline text-[#4F32FF] dark:text-[#B2FF5D] transition-all duration-150 sm:underline decoration-from-font decoration-wavy underline-offset-4 decoration-[#4F32FF]/0 dark:decoration-[#B2FF5D]/0 hover:decoration-[#4F32FF]/100 dark:hover:decoration-[#B2FF5D]/100'
           >
             CV
           </Link>
