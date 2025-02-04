26275038a14814fb2b3733e79d542236ef75064f
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-04 19:51:54 -0300

    refactor: move header and footer logic to layout

---
+++ src/app/[locale]/(main)/layout.tsx
@@ -0,0 +1,96 @@
+import { Separator } from '@/components/Separator'
+import { ToggleThemeButton } from '@/components/ThemeToggle'
+import { Link } from '@/i18n/routing'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className='min-h-dvh flex flex-col relative overflow-hidden'>
+      <header className='max-w-screen-2xl mx-auto w-full flex justify-between items-center p-8 sm:px-20 font-normal text-[#282828] dark:text-[#D3D3D3] text-sm'>
+        <div className='flex gap-2 items-center'>
+          <h1>MAËLLE.WORKS</h1>
+          <span>•</span>
+          <Link
+            href='/doc/cv_maelle.pdf'
+            className='no-underline text-[#4F32FF] dark:text-[#B2FF5D] transition-all duration-150 sm:underline decoration-from-font decoration-wavy underline-offset-4 decoration-[#4F32FF]/0 dark:decoration-[#B2FF5D]/0 hover:decoration-[#4F32FF]/100 dark:hover:decoration-[#B2FF5D]/100'
+          >
+            CV
+          </Link>
+        </div>
+
+        <div className='flex gap-5 items-center'>
+          <Link
+            // href={`/${locale === 'en' ? 'fr' : 'en'}`}
+            href='/fr'
+            className='size-5 relative group overflow-hidden flex'
+            aria-label='Toggle language'
+          >
+            <span className='absolute inset-0 transition-all duration-300 group-hover:translate-y-5 opacity-100 group-hover:opacity-0 leading-none self-center text-center'>
+              {/* {locale === 'en' ? 'EN' : 'FR'} */}
+              FR
+            </span>
+            <span className='absolute inset-0 transition-all duration-300 -translate-y-5 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 leading-none self-center text-center'>
+              {/* {locale === 'en' ? 'FR' : 'EN'} */}
+              EN
+            </span>
+          </Link>
+
+          <ToggleThemeButton />
+        </div>
+      </header>
+      <Separator className='relative left-1/2 -translate-x-1/2 w-screen' />
+
+      {children}
+
+      <Separator className='relative left-1/2 -translate-x-1/2 w-screen' />
+      <footer className='max-w-screen-2xl mx-auto w-full px-8 sm:px-20 font-normal text-[#282828] dark:text-[#D3D3D3] text-sm uppercase'>
+        <div className='py-8 flex flex-col gap-4 sm:items-center'>
+          <p>Travaillons ensemble!</p>
+
+          <div className='flex gap-2 items-center'>
+            <a
+              href='https://www.linkedin.com/in/maellems'
+              className='no-underline text-[#4F32FF] dark:text-[#B2FF5D] transition-all duration-150 sm:underline decoration-from-font decoration-wavy underline-offset-4 decoration-[#4F32FF]/0 dark:decoration-[#B2FF5D]/0 hover:decoration-[#4F32FF]/100 dark:hover:decoration-[#B2FF5D]/100'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              LinkedIn
+            </a>
+            <span>•</span>
+            <a
+              href='mailto:moisismaelle@gmail.com'
+              className='no-underline text-[#4F32FF] dark:text-[#B2FF5D] transition-all duration-150 sm:underline decoration-from-font decoration-wavy underline-offset-4 decoration-[#4F32FF]/0 dark:decoration-[#B2FF5D]/0 hover:decoration-[#4F32FF]/100 dark:hover:decoration-[#B2FF5D]/100'
+            >
+              moisismaelle@gmail.com
+            </a>
+          </div>
+        </div>
+
+        <Separator className='relative left-1/2 -translate-x-1/2 w-screen' />
+
+        <div className='py-8 flex flex-col gap-4 sm:flex-row sm:gap-0 sm:justify-between sm:items-center'>
+          <span>
+            Développé par{' '}
+            <a
+              href='https://erickgust.vercel.app/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='no-underline text-[#4F32FF] dark:text-[#B2FF5D] transition-all duration-150 hover:text-[#4F32FF] dark:hover:text-[#B2FF5D] sm:underline decoration-from-font decoration-wavy underline-offset-4 decoration-[#4F32FF]/0 dark:decoration-[#B2FF5D]/0 hover:decoration-[#4F32FF]/100 dark:hover:decoration-[#B2FF5D]/100'
+            >
+              Erick
+            </a>
+          </span>
+          <Link
+            href='/legal'
+            className='no-underline text-[#4F32FF] dark:text-[#B2FF5D] transition-all duration-150 sm:underline decoration-from-font decoration-wavy underline-offset-4 decoration-[#4F32FF]/0 dark:decoration-[#B2FF5D]/0 hover:decoration-[#4F32FF]/100 dark:hover:decoration-[#B2FF5D]/100'
+          >
+            mentions légales
+          </Link>
+        </div>
+      </footer>
+    </div>
+  )
+}
