fbccffbb43f5e8d736e8d601597a702c867ef1e0
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-06 19:21:23 -0300

    fix: update sitemap URL to use the correct domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://maelle.works/sitemap.xml',
+    sitemap: 'https://www.maelle.works/sitemap.xml',
   }
 }
