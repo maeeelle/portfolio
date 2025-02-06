06bc619484a3024c8d941c790924fb98172c059d
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-06 19:14:49 -0300

    feat: update sitemap and routing for French localization support and remove book

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,14 +5,50 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://www.maelle.works',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
     },
     {
-      url: 'https://www.maelle.works/doc/book.pdf',
+      url: 'https://www.maelle.works/doc/cv_maelleworks_en.pdf',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
+    },
+    {
+      url: 'https://www.maelle.works/doc/cv_maelleworks_fr.pdf',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/collection-zoologie-sorbonne',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/buddy',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/qsmp-shop',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/qsmp-co',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/maison-laurentine',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/ths-tracker',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/totoro',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/mon-coach-bescherelle',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.maelle.works/legal',
+      lastModified: new Date(),
     },
   ]
 }
