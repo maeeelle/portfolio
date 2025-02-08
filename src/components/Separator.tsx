c4a5e645248bcb480b1b4bfb693024bbb07e4209
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-08 14:52:29 -0300

    fix: add role='none' to Separator component

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -6,6 +6,7 @@ export function Separator({
 }: React.ComponentPropsWithoutRef<'div'>) {
   return (
     <div
+      role='none'
       className={clsx(
         'w-full h-px bg-[#E1E1E1] dark:bg-[#181818] shrink-0',
         className,
