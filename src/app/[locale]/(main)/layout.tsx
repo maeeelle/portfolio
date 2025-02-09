eb3bdfd502d9adf3390ccc790a23e066fd462390
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-09 11:21:40 -0300

    fix: possibly remove pdf error in production

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -24,6 +24,7 @@ export default async function Layout({ children, params }: LayoutProps) {
           </Link>
           <span aria-hidden>•</span>
           <NextLink
+            prefetch={false}
             hrefLang={locale}
             href={
               locale === 'en'
