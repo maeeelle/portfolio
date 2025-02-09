ac889aeb681e909c40624b9dcdcdc37c777adb56
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-09 19:37:38 -0300

    chore: remove crawling for resumes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/doc/',
     },
     sitemap: 'https://www.maelle.works/sitemap.xml',
   }
